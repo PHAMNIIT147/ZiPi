01d719f1c0e69a274be958cd2b592116c7a5a5db
PHAMNIIT147 <phongpham663@gmail.com>
2020-04-12 18:05:17 +0700

    pushpin update color for UI

--- lib/constant/app_colors.dart
+++ lib/constant/app_colors.dart
@@ -1,2 +1,5 @@
 import 'package:flutter/rendering.dart';
+const Color templateColor = Color.fromARGB(255, 31, 34, 41);
 const Color primaryColor = Color.fromARGB(255, 31, 229, 146);
+const Color drawnerColor = Color.fromARGB(100, 31, 34, 41);
+const Color cardColor = Color.fromARGB(100, 43, 49, 55);
