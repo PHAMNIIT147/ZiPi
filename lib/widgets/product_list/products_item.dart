beb36c3eb2976d3b66b4a26e8f5887271bb73a2e
PHAMNIIT147 <phongpham663@gmail.com>
2020-04-12 18:13:20 +0700

    :bug: fix problem image cover dislay on card

--- lib/widgets/product_list/products_item.dart
+++ lib/widgets/product_list/products_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:zipi/constant/app_colors.dart';
 import 'package:zipi/datamodels/product_item_models.dart';
 
 class ProductsItem extends StatelessWidget {
@@ -8,32 +9,41 @@ class ProductsItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: Colors.white,
+      color: cardColor,
       elevation: 2,
       child: SizedBox(
-        width: 360,
+        width: 300,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            SizedBox(
+              height: 200,
+              child: Image(
+                image: AssetImage(
+                  model.imageURL,
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
             Padding(
-              padding: const EdgeInsets.all(10),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10,
+                vertical: 10,
+              ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  SizedBox(
-                    height: 200,
-                    child: Image(
-                      image: AssetImage(model.imageURL),
-                    ),
-                  ),
                   Text(
                     model.title,
-                    style: TextStyle(fontSize: 13, fontWeight: FontWeight.w600),
+                    style: TextStyle(
+                      fontSize: 13,
+                      fontWeight: FontWeight.w600,
+                    ),
                     softWrap: true,
                   ),
                   Text(
                     '${model.duration} date',
-                    style: TextStyle(fontSize: 10),
+                    style: TextStyle(fontSize: 13),
                   ),
                 ],
               ),
